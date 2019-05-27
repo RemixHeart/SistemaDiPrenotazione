f4a3a7297f85f96b84afe27a2697bb2dd7512098
RemixHeart <bizou464@gmail.com>
2019-05-27 10:48:16 +0200

    recreating client

--- config/secret.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  database: 'mongodb://test:test123@ds135036.mlab.com:35036/sistemaprenotazione',
-  secret: 'secret'
-}
--- models/booking.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var BookingSchema = new Schema({
-  owner: { type: Schema.Types.ObjectId, ref: 'User'},
-  created: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Booking', BookingSchema)
